2feb5d5da1e559b6694f0eae4b70684150ad0867
jonasmachados <jonasmachado.ti@gmail.com>
2021-01-16 12:02:00 -0300

    JPA repository, dependency injection, database seeding

--- src/main/java/com/educandoweb/course2/resources/UserResource.java
+++ src/main/java/com/educandoweb/course2/resources/UserResource.java
@@ -1,9 +1,12 @@
-
 package com.educandoweb.course2.resources;
 
 import com.educandoweb.course2.entities.User;
+import com.educandoweb.course2.services.UserService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,12 +17,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-    
+
+    @Autowired //Anoatcao que  Associa a instancia 
+    private UserService service;
+
     //Metodo que retorna os usuarios
     @GetMapping
-    public ResponseEntity<User> findAll(){ //ResponseEntity: resposta de aquisicao web
-        User u = new User(11, "Marua Helena", "maria@gmail.com", "99999999", "12345");
-        return ResponseEntity.ok().body(u);//Contralador rest
+    public ResponseEntity<List<User>> findAll() { //ResponseEntity: resposta de aquisicao web
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);//Contralador rest
     }
-    
+
+    //Metodo que retorna os usuarios por ID
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Integer id) { //ResponseEntity: resposta de aquisicao web
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+
+    }
+
 }
---
+++ src/main/java/com/educandoweb/course2/services/UserService.java
@@ -0,0 +1,29 @@
+package com.educandoweb.course2.services;
+
+import com.educandoweb.course2.entities.User;
+import com.educandoweb.course2.repositories.UserRepository;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Jonas created 16/01/2021
+ */
+@Service //Resgiatra a classe como componente do Spring 
+public class UserService {
+
+    @Autowired //Anoatcao que  Associa a instancia 
+    private UserRepository repository;
+
+    public List<User> findAll() { //Operacao na camada de servico
+        return repository.findAll();
+    }
+
+    public User findById(Integer id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+
+}
